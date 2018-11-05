ef5e5bb89f814a88aa9d2c7823b052a0ceec0f4d
Eshum <eshumchronicles@gmail.com>
2018-11-05 10:42:37 +0100

    Add project files.

---
+++ Game/Game.fs
@@ -0,0 +1,34 @@
+﻿namespace Game
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+type AmbitionGame() as self = 
+    inherit Game()
+
+    let graphics: GraphicsDeviceManager = new GraphicsDeviceManager(self)
+
+    [<DefaultValue>]
+    val mutable spritebatch: SpriteBatch
+
+    do 
+        self.Content.RootDirectory <- "Content"
+
+    member this.X = "F#"
+
+    override self.Initialize() = 
+        base.Initialize()
+
+    override self.LoadContent() =
+        self.spritebatch <- new SpriteBatch(self.GraphicsDevice)
+        base.LoadContent()
+
+    override self.UnloadContent() = 
+        base.UnloadContent() 
+
+    override self.Update(time: GameTime) = 
+        base.Update time 
+    
+    override self.Draw(time: GameTime) = 
+        base.GraphicsDevice.Clear Color.DeepSkyBlue
+        base.Draw time 
---
+++ Launcher/Program.cs
@@ -0,0 +1,21 @@
+﻿using Game;
+using System;
+
+namespace Ambition
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new AmbitionGame())
+                game.Run();
+        }
+    }
+}
